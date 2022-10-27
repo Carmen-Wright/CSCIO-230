b301efc2c81a6ad9a7b4f2b8a369a292947cc81d
Carmen-Wright <99839370+Carmen-Wright@users.noreply.github.com>
2022-10-26 23:44:39 -0400

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -11,11 +11,35 @@
 
 
 
-char* readString(char* fileName);
+char* readString(char* fileName)
+  
+  FILE * textfile = fopen(fileName,"r");
+  char* readtext = malloc(MAX_LINE_LEN*sizeof(char));
+  strcpy(textfile,fgets(textfile,MAX_LINE_LEN-1,openfile));
+  fclose(readtext);
+
+  return textfile;
+  
+
+  
+  ;
 
 
 
+char* mysteryExplode(const char* str);
+ 
+ int length = strlen(str)-1; // Calculate size.
+ int explode = ((length*(length+1))/2);
+ char* outcome=calloc(explode, 1);  // Alocate space.
+ 
+ for (int a=0; a<length; a++) {
+    for(int a = 0; a <= b; a++){
+      strcpy(outcome, strncat(outcome,&str[a],1));
+    }
+ }
+ 
+ 
+ 
+return outcome;
+}
 
-
-
-char* readString(char* fileName);
