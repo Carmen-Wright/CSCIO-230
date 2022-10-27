38275de297a58b78966252b52cd4c98632060920
Carmen-Wright <99839370+Carmen-Wright@users.noreply.github.com>
2022-10-26 22:37:01 -0400

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -2,4 +2,20 @@
  * Implement your solution in thi file
  *
  */
+#include "lab1.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+
+
+
+
+char* readString(char* fileName);
+
+
+
+
+
+
+char* readString(char* fileName);
